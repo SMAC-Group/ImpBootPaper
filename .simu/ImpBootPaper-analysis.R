092b82577cc9518bd78c986255fce15156a2f8d6
samorso <Samuel.Orso@unige.ch>
2022-05-28 14:18:13 +0200

    Fix BCa

--- .simu/ImpBootPaper-analysis.R
+++ .simu/ImpBootPaper-analysis.R
@@ -12,11 +12,12 @@ model <- "treg"
 nu <- 3
 k <- seq(5,40,by=5)
 coverages <- ci_length <- matrix(nrow = length(method_name), ncol = length(k))
-id <- 3:4
+id <- 11:12
+beta0 <- 0
 
 for(i in seq_along(k)){
   load(file = paste0(".simu/data/",model,"_n_250_p_",k[i],"_nu_",nu,".rds"))
-  coverages[,i] <- sapply(res[1:5], function(x) mean(apply(x,1,function(y){check_ci(ci=y[id],theta=-1)})=="center",na.rm=T))
+  coverages[,i] <- sapply(res[1:5], function(x) mean(apply(x,1,function(y){check_ci(ci=y[id],theta=beta0)})=="center",na.rm=T))
   ci_length[,i] <- sapply(res[1:5], function(x) median(apply(x,1,function(y){diff(y[id])}),na.rm=T))
 }
 
@@ -40,7 +41,7 @@ p <- ggplot(ds, aes(x = Dimension, y = Coverage, col = Method)) +
   geom_path() +
   theme_bw() +
   theme(text = element_text(size = 20), plot.title = element_text(hjust = 0.5)) +
-  ggtitle(paste0("Parameter: beta1\nSample size: 250\nDegrees of freedom: ",nu)) +
+  ggtitle(paste0("Parameter: beta=",beta0,"\nSample size: 250\nDegrees of freedom: ",nu)) +
   xlab("p")
 p
 
--- .simu/real_data_analysis.R
+++ .simu/real_data_analysis.R
@@ -1,20 +1,81 @@
 library(ImpBootPaper)
 library(idar)
+library(MASS)
+library(VGAM)
 data("codex")
 
-y <- scale(codex$auc)
+robscale <- function(x, center = TRUE, scale = TRUE) UseMethod("robscale")
+robscale.default <- function(x, center = TRUE, scale = TRUE){
+  ifelse(center,m<-median(x, na.rm = TRUE),m<-0)
+  ifelse(scale,s<-mad(x, na.rm=TRUE),s<-1)
+  (x - m) / s
+}
+robscale.matrix <- function(x, center = TRUE, scale = TRUE){
+  apply(x,MARGIN=2,FUN=robscale.default)
+}
+
+# variable selection
+selected_variables <- c("gender", "bmi", "t1_2", "age")
+interaction <- list(c("gender", "bmi"))
+y <- robscale(codex$auc)
+# y <- scale(codex$auc)
 intercept <- rep(1, nrow(codex))
-x <- as.matrix(cbind(intercept, subset(codex, select=c("gender", "age", "bmi"))))
-x[,3:4] <- scale(x[,3:4])
-param <- list(beta = rep(0,4),
-              sig2 = 4,
+x <- as.matrix(subset(codex, select=selected_variables))
+x[,selected_variables[-1]] <- robscale(x[,selected_variables[-1]])
+if(length(interaction)!=0){
+  inter <- sapply(interaction, FUN=function(y) apply(x[,y],1,prod))
+  colnames(inter) <- sapply(interaction, paste, collapse = "_")
+  x <- cbind(x, inter)
+}
+
+# modeling
+fit_lm <- lm(y~x)
+fit_lmrob <- rlm(y~x)
+start <- c(coef(fit_lmrob), log(sigma(fit_lmrob)), log(30))
+fit_vgam <- vglm(y ~ x, family = studentt3(ldf = "loglink"), coefstart = start)
+x <- cbind(intercept,x)
+param <- list(beta = coef(fit_lmrob),
+              sig2 = sigma(fit_lmrob)^2,
               nu = 30)
+control <- list(seed = 4987, nc = 1L)
 
 fit_sample <- mle_estimate(y, x, unlist(param), "treg")
 theta_hat <- fit_sample$par
-implicit <- impboot_interval(y, x, theta_hat, "treg", control = list(seed = 498732, nc = 4L))
-boot <- parboot(y, x, theta_hat, "treg", control = list(seed = 498732, nc = 4L))
-percentile <- parboot_interval(y, x, theta_hat, boot, "treg")
-boott <- parboott_interval(y, x, theta_hat, "treg", boot = boot, control = list(seed = 498732, nc = 4L))
-bca <- BCa_interval(y=y, x=x, initial=theta_hat, model="treg", boot = boot, control = list(seed = 498732))
-classic <- asymptotic_interval(y, x, theta_hat, "treg", boot = boot, control = list(seed = 498732, nc = 4L))
+
+res <- cbind(coef(fit_lm),coef(fit_lmrob),coef(fit_vgam,matrix=T)[,1],head(theta_hat,-2))
+colnames(res) <- c("LM", "RLM", "VGAM", "treg")
+res
+
+extra <- cbind(c(exp(coef(fit_vgam,matrix=T)[1,2])^2,exp(coef(fit_vgam,matrix=T)[1,3])),tail(theta_hat,2))
+colnames(extra) <- c("VGAM", "treg")
+rownames(extra) <- c("sig2", "nu")
+extra
+
+resid <- c(y - tcrossprod(head(theta_hat,-2), x))
+qqnorm(resid)
+qqline(resid, col = 2)
+qqplot(resid, sqrt(theta_hat[length(theta_hat)-1]) * rt(length(y), df = theta_hat[length(theta_hat)]))
+
+# confidence intervals
+class_lm <- confint(fit_lm)
+class_vgam <- confint(fit_vgam)
+
+implicit <- t(impboot_interval(y, x, theta_hat, "treg", control = control))
+rownames(implicit) <- c(colnames(x), "sig2", "nu")
+implicit
+
+boot <- parboot(y, x, theta_hat, "treg", control = control)
+percentile <- t(parboot_interval(y, x, theta_hat, boot, "treg"))
+rownames(percentile) <- c(colnames(x), "sig2", "nu")
+percentile
+
+boott <- t(parboott_interval(y, x, theta_hat, "treg", boot = boot, control = control))
+rownames(boott) <- c(colnames(x), "sig2", "nu")
+boott
+bca <- t(BCa_interval(y=y, x=x, initial=theta_hat, model="treg", boot = boot, control = control))
+rownames(bca) <- c(colnames(x), "sig2", "nu")
+bca
+classic <- t(asymptotic_interval(y, x, theta_hat, "treg", boot = boot, control = control))
+rownames(classic) <- c(colnames(x), "sig2", "nu")
+classic
+
